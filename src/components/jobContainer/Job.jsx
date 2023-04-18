a67b044294f99606aad054bbe1c8fc4aad442b50
Diego Cuevas <ingdcuevas@gmail.com>
2023-04-17 23:47:32 -0500

    component jobs - desktop version almost finished

--- src/components/jobContainer/Job.jsx
+++ src/components/jobContainer/Job.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import styles from './job.modules.css'
+import React, { useState } from "react";
+import styles from "./job.modules.css";
 
-const Job = ({data}) => {
-
-  console.log(data)
-  return (
-    data?.map((item, index) => (
-      <article key={index}>
-        <div>
+const Job = ({ data }) => {
+  return data?.map((item, index) => (
+    <article
+      className={`container ${item.featured ? "border-left" : undefined}`}
+      key={index}
+    >
+      <div className="left-side">
+        <div className="image-container">
           <img src={item.logo} alt={item.company} />
         </div>
-      </article>
-    ))
-  )
-}
+        <div>
+          <div className="info-container">
+            <span className="first-span">{item.company}</span>
+            <span className={item.new ? "second-span" : undefined}>
+              {item.new ? "New!" : undefined}
+            </span>
+            <span className={item.featured ? "third-span" : undefined}>
+              {item.featured ? "Featured" : undefined}
+            </span>
+          </div>
 
-export default Job
+          <p className="position-container">{item.position}</p>
+
+          <div className="features-container">
+            <ul>
+              <li className="no-list">{item.postedAt}</li>
+              <li>{item.contract}</li>
+              <li>{item.location}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="right-side">
+        <button>{item.role}</button>
+        <button>{item.level}</button>
+        {[...item.languages, ...item.tools].map((item, id) => (
+          <button key={id}>{item}</button>
+        ))}
+      </div>
+    </article>
+  ));
+};
+
+export default Job;
